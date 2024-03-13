379b92f84be746ade8c61e3e6e9ee70a03d65f1e
chibrik <ilya.ismailau@gmail.com>
2024-03-13 22:08:11 +0100

    added 8ball

---
+++ beginning/data-types.rb
@@ -0,0 +1,34 @@
+#integer
+x = 5
+c = 15
+
+#float
+a = 3.1
+b = 2.3
+
+#string
+f = 'dima'
+t = 'boris'
+ 
+#array
+g = ['boss', 2.5]
+u = [1.2,'smog',5]
+
+#hash
+d = {'mail'=>'is@gmail.com'}
+v = {'mail'=>'ii@gmail.com'}
+
+#boolean
+error_es = false
+is7 = true
+
+#range
+e = 'w'..'h'
+q = 1..123
+
+#nil
+j = nil 
+p  error_es
+puts x*a
+puts c/b
+puts g+u
--- beginning/hello.rb
+++ beginning/hello.rb
@@ -1,2 +1,2 @@
-puts 'hello,world!'
-puts 'Goodbye,world'
+puts 'dmitri,bye!'
+p 'dmitri hi!'
